05b029e61e29aac0ccecbc5496562c6345a04c9f
kristjanjansen <kristjan.jansen@gmail.com>
2021-02-03 14:58:30 +0200

    clean

--- index.js
+++ index.js
@@ -13,27 +13,36 @@ export const unique = (arr) => [...new Set(arr)];
 const App = {
   setup() {
     const width = ref(1000);
-    const height = ref(5000);
+    const height = ref(7000);
 
     const sourceData = ref([]);
     fetch("./structure.json")
       .then((res) => res.json())
       .then((res) => (sourceData.value = res));
 
-    const tree = d3.tree().size([height.value, width.value]);
+    const tree = d3
+      .tree()
+      .size([height.value, width.value])
+      .separation((a, b) => {
+        return a.parent.depth === 1 ? 4 : 1;
+      });
+
     const data = computed(() => {
       const children = sourceData.value.map((d) => {
         const domains = unique(d.related.map((r) => r.subdomain));
         return {
           name: d.title,
-          // children: d.related.map((r) => r.title),
+          url: d.url,
           children: domains.map((domain) => {
             const relatedDomains = d.related.filter(
               (r) => r.subdomain === domain
             );
             return {
               name: domain,
-              children: relatedDomains.map((r) => r.title),
+              children: relatedDomains.map((r) => ({
+                name: r.title,
+                url: r.url,
+              })),
             };
           }),
         };
@@ -44,11 +53,6 @@ const App = {
         children,
       });
     });
-
-    const link = d3
-      .linkHorizontal()
-      .x((d) => d.parent.x)
-      .y((d) => d.parent.y);
 
     const elements = computed(() =>
       flattenTree(tree(data.value), "children").map((el) => {
@@ -66,30 +70,16 @@ const App = {
 
     watchEffect(() => console.log(elements.value));
 
-    //const root = d3.hierarchy(data);
-    //  console.log(root.descendants());
-    // const t = root.descendants().map((d) => ({
-    //   node: tree(d),
-    //   d,
-    //   //children: d.descendants().map((d) => ({ node: tree(d) })),
-    // }));
-
     return { elements, width, height };
   },
   template: `
   <svg :width="width" :height="height">
-  <path 
-    v-for="el in elements"
-    :d="el.path"
-    stroke="#bbb"
-    fill="none"
-  />
-  <!-- <text 
-    v-for="el in elements"
-    :x="el.y"
-    :y="el.x"
-    fill="black"
-  >{{el.data.name || el.name}}</text> -->
+    <path 
+      v-for="el in elements"
+      :d="el.path"
+      stroke="#ddd"
+      fill="none"
+    />
   </svg>
   <div
     v-for="el in elements"
@@ -102,67 +92,11 @@ const App = {
       transform: 'translateY(-0.6em)'
     }"
   >
+    <component :is="el.data.url ? 'a' : 'div'" :href="el.data.url" target="_blank">
     {{ el.data.name ? el.data.name : el.data }}
+    </component>
   </div>
   `,
 };
 
 createApp(App).mount("#app");
-
-/*
-
-
-    <!-- <div 
-      v-for="l in root"
-      :style="{
-        border: '1px solid red',
-        position: 'fixed',
-        right: tree(l).x + 'px',
-        top: tree(l).y + 'px',
-        width: '100px',
-        height: '100px'
-      }"
-    >
-      {{ tree(l).name }}
-    </div> -->
-    <!-- <div 
-      v-for="(l,i) in root.descendants()"
-      :style="{
-        border: '1px solid red',
-        position: 'fixed',
-        right: tree(l).x + 'px',
-        top: tree(l).y + 'px',
-        width: '100px',
-        height: '100px'
-      }"
-    >
-      a{{ i }}
-    </div> -->
-  `,
-  */
-
-/*
-
-import { createApp } from "https://unpkg.com/vue@3.0.0/dist/vue.esm-browser.prod.js";
-import * as d3 from "https://cdn.skypack.dev/d3";
-//import * as vue3tree from "https://cdn.skypack.dev/vued3tree";
-//const { tree: Tree } = vue3tree.default;
-
-const App = {
-  //components: { Tree },
-  async setup() {
-    const sourceData = await fetch("./structure.json").then((res) =>
-      res.json()
-    );
-    const data = sourceData.map((d) => ({
-      name: d.title,
-      children: d.related.map((c) => c.title),
-    }));
-    //return { data };
-  },
-  template: `Hello`,
-};
-
-createApp(App).mount("#app");
-
-*/
